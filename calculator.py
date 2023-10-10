eae07f83be99d8cc27c30b253485687eef31d452
loafer <gysik1234@gmail.com>
2023-10-10 22:12:30 +0500

    firs calculator

--- calculator.py
+++ calculator.py
@@ -1,20 +1,21 @@
-# Оптимизированный способ
-num1 = int(input("Введите первое целое число: "))
-num2 = int(input("Введите второе целое число: "))
-operation = input("Введите математическую операцию (+, -, *, /): ")
+# калькулятор
+num1 = int(input("введите первое целое число: "))
+num2 = int(input("введите второе целое число: "))
+operation = input("введите математическую операцию (+, -, *, /): ")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 == 0:
+        print("деление на ноль невозможно")
+    else:
+        result = num1 / num2
+else:
+    print("неверная операция")
 
 if operation in "+-*/":
-    if operation == "+":
-        result = num1 + num2
-    elif operation == "-":
-        result = num1 - num2
-    elif operation == "*":
-        result = num1 * num2
-    elif operation == "/":
-        if num2 == 0:
-            print("На ноль делить нельзя!")
-        else:
-            result = num1 / num2
-    print("Результат:", result)
-else:
-    print("Неверная операция")
+    print("результат:", result)
